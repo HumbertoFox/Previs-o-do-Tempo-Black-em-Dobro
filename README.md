19ca57f5fc288d3a2fbac3d9d6ad62facb67efe3
Humberto Ribeiro <betofoxmal@gmail.com>
2023-11-24 16:34:55 -0300

    Ajuste indentação html e css, js mudando background pela hora

---
+++ README.md
@@ -0,0 +1,34 @@
+# projeto-Previs-do-Tempo-fetch-Weather-Api
+
+### Repositório contendo os arquivos iniciais do projeto de Fetch e GitHub API!
+
+Os desafios do DevQuest ajudam você a melhorar suas habilidades de codificação criando projetos realistas.
+
+## Layout do projeto em tela de Desktop/Notebook/Tablet/Mobile.
+
+<div align="center">
+
+  <img src="" width="400px"/>
+
+</div>
+
+### O que aprendi
+
+```Js
+
+```
+
+### Construído com
+
+- Marcação semântica HTML5
+- Propriedades personalizadas CSS
+- Caixa flexível
+- Js com Fetch e GitHub API
+
+## Desenvolvido em:
+
+<div>
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width="30px"/>
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width="30px"/>
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" width="30px"/>
+</div>
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const chaveDoApi = "cd31863ae267407cb01212131232011";
 const botaoDeBusca = document.querySelector(".btn-busca");
+const selectMainContainer = document.querySelector("#container");
 
 setInterval(() => {
     getHours()
@@ -18,30 +19,40 @@ const getHours = () => {
     const second = seconds < 10 ? `0${seconds}` : seconds;
     clock.innerHTML = `${hour}:${minute}:${second}`;
     dataAtual.innerHTML = `${year}`;
+
+    if (hour <= 5) {
+        selectMainContainer.style.backgroundImage = "url(./src/images/background-0.png)";
+    } else if (hour >= 6 && hour <= 11) {
+        selectMainContainer.style.backgroundImage = "url(./src/images/background-1.png)";
+    } if (hour >= 12 && hour <= 17) {
+        selectMainContainer.style.backgroundImage = "url(./src/images/background-2.png)"
+    } else if (hour >= 18 && hour <= 23) {
+        selectMainContainer.style.backgroundImage = "url(./src/images/background-3.png)"
+    };
 };
 
 botaoDeBusca.addEventListener("click", async () => {
     const cidade = document.getElementById("input-busca").value;
 
-    if(!cidade) return;
+    if (!cidade) return;
 
     const dados = await buscarDadosDaCidade(cidade);
-    
-    if(dados) peencherDadosNaTela(dados, cidade);
+
+    if (dados) peencherDadosNaTela(dados, cidade);
 });
 
-async function buscarDadosDaCidade(cidade){
+async function buscarDadosDaCidade(cidade) {
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDoApi}&q=${cidade}&aqi=no&lang=pt`;
     const response = await fetch(apiUrl);
 
-    if(response.status !== 200) return;
+    if (response.status !== 200) return;
 
     const dados = response.json();
-    
+
     return dados;
 };
 
-function peencherDadosNaTela(dados, cidade){
+function peencherDadosNaTela(dados, cidade) {
     const tempetatura = dados.current.temp_c;
     const condicao = dados.current.condition.text;
     const humidade = dados.current.humidity;
